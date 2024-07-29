61f9747185866dd1994a9e16f61ce21d5ff36044
MohamedAklamaash <aklamaash78@gmail.com>
2024-07-29 20:31:56 +0530

    sniffed network packets

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+var DevName = "lo"
+var Found = false
 
 func main() {
-	fmt.Println("Welcome to  Network Intrusion Detections System!")
+	devices, err := pcap.FindAllDevs() // finding network interfaces for packet capturing
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, device := range devices {
+		if device.Name == DevName {
+			Found = true
+		}
+		fmt.Println(device)
+	}
+	if !Found {
+		log.Fatal("desired device not found")
+	}
+	handle, err := pcap.OpenLive(DevName, 1600, false, pcap.BlockForever)
+	if err != nil {
+		log.Fatal("error in opening live")
+	}
+	defer handle.Close()
+	// berkeley packet filter for filtering https traffic
+	if err := handle.SetBPFFilter("tcp and port 21"); err != nil {
+		log.Panicln(err)
+	}
+	source := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range source.Packets() {
+		appLayer := packet.ApplicationLayer()
+		if appLayer != nil {
+			data := appLayer.Payload()
+
+			if bytes.Contains(data, []byte("USER")) || bytes.Contains(data, []byte("PASS")) {
+				fmt.Println(string(data))
+			}
+		}
+		// fmt.Println(packet)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/MohamedAklamaash
 
 go 1.22.4
+
+require github.com/google/gopacket v1.1.19
+
+require golang.org/x/sys v0.22.0 // indirect
